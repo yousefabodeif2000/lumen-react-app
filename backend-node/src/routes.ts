4d9d1cf65d25309c7aa3aa447b645d2c9cb12dd4
yousefabodeif2000 <83887183+yousefabodeif2000@users.noreply.github.com>
2025-09-06 14:46:21 +0300

    Cached responses with 60s TTL to reduce API calls.

--- backend-node/src/routes.ts
+++ backend-node/src/routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { getPosts, getPostsByID, pingLumen, setAuthToken } from './services/lumenAPI';
-
+import { redis } from './redisClient';
 const router = Router();
 
 router.get('/ping-lumen', async (req, res) => {
@@ -14,26 +14,43 @@ router.get('/ping-lumen', async (req, res) => {
 
 router.get('/posts', async (req, res) => {
   try {
+    const cacheKey = 'posts_cache';
+    const cachedPosts = await redis.get(cacheKey);
     const token = req.headers['authorization']; 
     if (token) {
       setAuthToken(token.replace('Bearer ', ''));
     }
+    if (cachedPosts) {
+      return res.json(JSON.parse(cachedPosts));
+    }
     const result = await getPosts();
+    await redis.set(cacheKey, JSON.stringify(result.data), 'EX', 60);
+
     res.json(result.data);
-  } catch (err: any) {
+
+  } 
+  catch (err: any) {
     res.status(500).json({ error: err.message });
   }
 });
 
 router.get('/posts/:id', async (req, res) => {
   try {
+
+    const postId = Number(req.params.id);
+    const cacheKey = `post_${postId}_cache`;
     const token = req.headers['authorization']; 
+    const cachedPost = await redis.get(cacheKey);
+
     if (token) {
       setAuthToken(token.replace('Bearer ', ''));
     }
-
-    const postId = Number(req.params.id);
+    if(cachedPost) {
+      return res.json(JSON.parse(cachedPost));
+    }
+    
     const result = await getPostsByID(postId);
+    await redis.set(cacheKey, JSON.stringify(result.data), 'EX', 60);
     res.json(result.data);
   } catch (err: any) {
     res.status(500).json({ error: err.message });
