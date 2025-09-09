5bea1fb6d393520c5d161915c92336db2b705d58
yousefabodeif2000 <83887183+yousefabodeif2000@users.noreply.github.com>
2025-09-09 03:46:31 +0300

    CI/CD Workflow

--- backend-lumen/tests/TestCase.php
+++ backend-lumen/tests/TestCase.php
@@ -4,9 +4,12 @@ namespace Tests;
 
 use Laravel\Lumen\Testing\TestCase as BaseTestCase;
 use Illuminate\Support\Facades\Artisan;
+use Laravel\Lumen\Testing\DatabaseMigrations;
 
 abstract class TestCase extends BaseTestCase
 {
+    use DatabaseMigrations;
+    
     public function createApplication()
     {
         return require __DIR__ . '/../bootstrap/app.php';
