eadd60089392ca809b80cbbb5ba2240b19768aa8
yousefabodeif2000 <83887183+yousefabodeif2000@users.noreply.github.com>
2025-09-09 03:21:04 +0300

    CI/CD Workflow

--- backend-lumen/tests/TestCase.php
+++ backend-lumen/tests/TestCase.php
@@ -3,16 +3,20 @@
 namespace Tests;
 
 use Laravel\Lumen\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Artisan;
 
 abstract class TestCase extends BaseTestCase
 {
-    /**
-     * Creates the application.
-     *
-     * @return \Laravel\Lumen\Application
-     */
     public function createApplication()
     {
-        return require __DIR__.'/../bootstrap/app.php';
+        return require __DIR__ . '/../bootstrap/app.php';
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Refresh DB before each test
+        Artisan::call('migrate:fresh');
     }
 }
